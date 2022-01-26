42434523e901dcace5e0c4c215b359731897eda9
[umaidila] <[frenirus125@gmail.com]>
2022-01-26 21:21:10 +1000

    init

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-
 from client import Client
 
 if __name__ == '__main__':
-
     client = Client()
 
     print(client.setToken("semen", "password"))
     print(client.getFiles())
-    print(client.delFile(4))
+    print(client.addFile('temp.png'))
     print(client.getFiles())
-
+    # print(client.delFile(9)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
